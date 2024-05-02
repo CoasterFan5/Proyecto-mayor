f48806a59433c795d326f82ff63658ed2589cb50
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2024-05-02 05:43:27 -0400

    add some animation

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public float speedFactor = 5f;
     public float verticalSpeedFactor = 2000f;
     private Rigidbody2D rigidBody;
+    private Animator animator;
 
     public float spawnX;
     public float spawnY;
@@ -30,12 +31,20 @@ public class PlayerMovement : MonoBehaviour
 
     private void Start()
     {
+        animator = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody2D>();
     }
     private void FixedUpdate()
     {
-        
-        rigidBody.velocity = new Vector2(Input.GetAxis("Horizontal") * speedFactor, rigidBody.velocity.y);
+        var horizontalAxisInput = Input.GetAxis("Horizontal");
+        if(horizontalAxisInput != 0)
+        {
+            animator.SetBool("isWalking", true);
+        } else
+        {
+            animator.SetBool("isWalking", false);
+        }
+        rigidBody.velocity = new Vector2(horizontalAxisInput * speedFactor, rigidBody.velocity.y);
         if(IsGrounded() && Input.GetAxis("Vertical") > 0)
         {
             Debug.Log("Adding vertical force");
